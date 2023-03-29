379ccf079b01e4caf3ed9c99418d43adb575cf3e
sonchv123 <64958362+sonchv123@users.noreply.github.com>
2023-03-29 14:39:18 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(view -> {
+
+            Intent i = new Intent(getApplicationContext(),IntroActivity.class);
+            startActivity(i);
+
+        });
+    }
+}
